448750e6a110d0d9ffe3913f99076155bfe0bd27
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2025-06-24 15:55:45 +0200

    Allow finding vulnerabilities

--- src/regcertipy/__init__.py
+++ src/regcertipy/__init__.py
@@ -1,8 +1,45 @@
 import argparse
 
 from certipy.lib.formatting import pretty_print
+from certipy.commands.find import Find
 from regcertipy.models import CertTemplate
 from regcertipy.parsers import RegfileParser
+from datetime import datetime
+from .utils import sid_to_name
+from collections import OrderedDict
+
+
+class MockTarget:
+    username = None
+
+
+class MockLDAPConnection:
+    user_sids = []
+
+    def __init__(self, sid_file):
+        if sid_file:
+            with open(sid_file) as f:
+                for line in f:
+                    self.user_sids.append(line[:-1])
+
+    def get_user_sids(self, *args, **kwargs):
+        return self.user_sids
+
+    def lookup_sid(self, sid, **kwargs):
+        return {"name": sid_to_name(sid)}
+
+
+class MyFind(Find):
+    def get_template_properties(self, template, template_properties):
+        template_properties = super().get_template_properties(
+            template, template_properties
+        )
+        for key in ["Template OIDs"]:
+            template_oids = template.get(key)
+            if template_oids:
+                template_properties[key] = template_oids
+
+        return template_properties
 
 
 def main():
@@ -12,6 +49,7 @@ def main():
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument("regfile", help="Path to the .reg file.")
+    parser.add_argument("-s", "--sid-file", help="File containing the user's SIDs")
     args = parser.parse_args()
 
     parser = RegfileParser(args.regfile)
@@ -32,8 +70,20 @@ def main():
 
     print(f"[*] Found {len(templates)} templates in the registry")
 
+    templates = [template.to_dict() for template in templates]
+
+    find = MyFind(
+        target=MockTarget(),
+        connection=MockLDAPConnection(args.sid_file),
+        stdout=True,
+        text=True,
+    )
     for template in templates:
-        pretty_print(template.to_dict())
+        user_can_enroll, enrollable_sids = find.can_user_enroll_in_template(template)
+        template.set("Can Enroll", user_can_enroll)
+        template.set("Enrollable SIDs", [sid_to_name(sid) for sid in enrollable_sids])
+    prefix = datetime.now().strftime("%Y%m%d%H%M%S")
+    find._save_output(templates=templates, cas=[], oids=[], prefix=prefix)
 
 
 if __name__ == "__main__":
--- src/regcertipy/models/cert_template.py
+++ src/regcertipy/models/cert_template.py
@@ -7,9 +7,19 @@ from certipy.lib.constants import (
     EXTENDED_RIGHTS_NAME_MAP,
     CertificateNameFlag,
     EnrollmentFlag,
+    PrivateKeyFlag,
     OID_TO_STR_MAP,
 )
 from certipy.lib.security import CertificateSecurity
+from certipy.lib.ldap import LDAPEntry
+
+
+class MockLDAPEntry(LDAPEntry):
+    def __init__(self, attributes):
+        self.attributes = attributes
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
 
 
 class CertTemplate:
@@ -18,6 +28,11 @@ class CertTemplate:
 
         self.name = name
         self.display_name = self.data["DisplayName"]
+        self.schema_version = self.data["msPKI-Template-Schema-Version"]
+        if self.schema_version:
+            self.schema_version = int(self.schema_version)
+        else:
+            self.schema_version = 1
         self.oid = (
             self.data["msPKI-Cert-Template-OID"].decode("utf-16-le").rstrip("\0\0")
         )
@@ -26,12 +41,31 @@ class CertTemplate:
         self.name_flags = CertificateNameFlag(self.data["msPKI-Certificate-Name-Flag"])
 
         self.enrollment_flags = EnrollmentFlag(self.data["msPKI-Enrollment-Flag"])
+        self.private_key_flag = PrivateKeyFlag(self.data["msPKI-Private-Key-Flag"])
         self.signatures_required = self.data["msPKI-RA-Signature"]
 
         self.extended_key_usage = list(
             map(
                 lambda x: OID_TO_STR_MAP[x] if x in OID_TO_STR_MAP else x,
                 data["ExtKeyUsageSyntax"]
+                .decode("utf-16-le")
+                .rstrip("\0\0")
+                .split("\0"),
+            )
+        )
+        self.application_policies = list(
+            map(
+                lambda x: OID_TO_STR_MAP[x] if x in OID_TO_STR_MAP else x,
+                data["msPKI-RA-Application-Policies"]
+                .decode("utf-16-le")
+                .rstrip("\0\0")
+                .split("\0"),
+            )
+        )
+        self.issuance_policies = list(
+            map(
+                lambda x: OID_TO_STR_MAP[x] if x in OID_TO_STR_MAP else x,
+                data["msPKI-Certificate-Policy"]
                 .decode("utf-16-le")
                 .rstrip("\0\0")
                 .split("\0"),
@@ -101,16 +135,47 @@ class CertTemplate:
 
         return permissions
 
+    @property
+    def any_purpose(self):
+        return "Any Purpose" in self.extended_key_usage
+
     def to_dict(self):
-        return {
-            "Name": self.name,
-            "Friendly Name": self.display_name,
-            "Template OID": self.oid,
-            "Validity Period": self.validity_period,
-            "Renewal Period": self.renewal_period,
-            "Name Flags": self.name_flags,
-            "Enrollment Flags": self.enrollment_flags,
-            "Signatures Required": self.signatures_required,
-            "Extended Key Usage": self.extended_key_usage,
-            "Permissions": self.permissions,
-        }
+        return MockLDAPEntry(
+            {
+                "cn": self.name,
+                "displayName": self.display_name,
+                "Template OID": self.oid,
+                "validity_period": self.validity_period,
+                "renewal_period": self.renewal_period,
+                "certificate_name_flag": self.name_flags,
+                "enrollment_flag": self.enrollment_flags,
+                "authorized_signatures_required": self.signatures_required,
+                "extended_key_usage": self.extended_key_usage,
+                "Permissions": self.permissions,
+                "nTSecurityDescriptor": self.data["Security"],
+                "enrollee_supplies_subject": CertificateNameFlag.ENROLLEE_SUPPLIES_SUBJECT
+                in self.name_flags,
+                "enrollment_agent": "Certificate Request Agent"
+                in self.extended_key_usage,
+                "any_purpose": self.any_purpose,
+                "client_authentication": self.any_purpose
+                or any(
+                    eku in self.extended_key_usage
+                    for eku in [
+                        "Client Authentication",
+                        "Smart Card Logon",
+                        "PKINIT Client Authentication",
+                    ]
+                ),
+                "private_key_flag": self.private_key_flag,
+                "requires_manager_approval": EnrollmentFlag.PEND_ALL_REQUESTS
+                in self.enrollment_flags,
+                "requires_key_archival": PrivateKeyFlag.REQUIRE_PRIVATE_KEY_ARCHIVAL
+                in self.private_key_flag,
+                "application_policies": self.application_policies,
+                "schema_version": self.schema_version,
+                "msPKI-Minimal-Key-Size": self.data["msPKI-Minimal-Key-Size"],
+                "msPKI-Certificate-Policy": self.issuance_policies,
+                "enabled": True,
+            }
+        )
