e8df39d5963f06ff5b954db938edc22acaa665aa
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2025-06-24 16:47:29 +0200

    Directly use certipy functionality instead of copying
    1. Remove build permissions method since this functionality is done by certipy itself
    2. Use WELLKNOWN_* from certipy

--- src/regcertipy/models/cert_template.py
+++ src/regcertipy/models/cert_template.py
@@ -1,16 +1,12 @@
 from typing import Dict
 
-import regcertipy.utils
 from certipy.commands.find import filetime_to_str
 from certipy.lib.constants import (
-    CertificateRights,
-    EXTENDED_RIGHTS_NAME_MAP,
     CertificateNameFlag,
     EnrollmentFlag,
     PrivateKeyFlag,
     OID_TO_STR_MAP,
 )
-from certipy.lib.security import CertificateSecurity
 from certipy.lib.ldap import LDAPEntry
 
 
@@ -72,69 +68,6 @@ class CertTemplate:
             )
         )
 
-        self.permissions = self._build_permissions(self.data["Security"])
-
-    @staticmethod
-    def _build_permissions(security_dict: Dict):
-        security = CertificateSecurity(security_dict)
-
-        enrollment_permissions = {}
-        enrollment_rights = []
-        all_extended_rights = []
-
-        permissions = {}
-
-        for sid, rights in security.aces.items():
-            if (
-                EXTENDED_RIGHTS_NAME_MAP["Enroll"] in rights["extended_rights"]
-                or EXTENDED_RIGHTS_NAME_MAP["AutoEnroll"] in rights["extended_rights"]
-            ):
-                enrollment_rights.append(regcertipy.utils.sid_to_name(sid))
-            if (
-                EXTENDED_RIGHTS_NAME_MAP["All-Extended-Rights"]
-                in rights["extended_rights"]
-            ):
-                all_extended_rights.append(regcertipy.utils.sid_to_name(sid))
-
-        if len(enrollment_rights) > 0:
-            enrollment_permissions["Enrollment Rights"] = enrollment_rights
-
-        if len(all_extended_rights) > 0:
-            enrollment_permissions["All Extended Rights"] = all_extended_rights
-
-        if len(enrollment_permissions) > 0:
-            permissions["Enrollment Permissions"] = enrollment_permissions
-
-        object_control_permissions = {"Owner": security.owner}
-
-        rights_mapping = [
-            (CertificateRights.GENERIC_ALL, [], "Full Control Principals"),
-            (CertificateRights.WRITE_OWNER, [], "Write Owner Principals"),
-            (CertificateRights.WRITE_DACL, [], "Write Dacl Principals"),
-            (
-                CertificateRights.WRITE_PROPERTY,
-                [],
-                "Write Property Principals",
-            ),
-        ]
-
-        for sid, rights in security.aces.items():
-            rights = rights["rights"]
-            sid = regcertipy.utils.sid_to_name(sid)
-
-            for right, principal_list, _ in rights_mapping:
-                if right in rights:
-                    principal_list.append(sid)
-
-        for _, rights, name in rights_mapping:
-            if len(rights) > 0:
-                object_control_permissions[name] = rights
-
-        if len(object_control_permissions) > 0:
-            permissions["Object Control Permissions"] = object_control_permissions
-
-        return permissions
-
     @property
     def any_purpose(self):
         return "Any Purpose" in self.extended_key_usage
@@ -151,7 +84,6 @@ class CertTemplate:
                 "enrollment_flag": self.enrollment_flags,
                 "authorized_signatures_required": self.signatures_required,
                 "extended_key_usage": self.extended_key_usage,
-                "Permissions": self.permissions,
                 "nTSecurityDescriptor": self.data["Security"],
                 "enrollee_supplies_subject": CertificateNameFlag.ENROLLEE_SUPPLIES_SUBJECT
                 in self.name_flags,
--- src/regcertipy/utils.py
+++ src/regcertipy/utils.py
@@ -1,29 +1,12 @@
+from certipy.lib.constants import WELLKNOWN_SIDS, WELLKNOWN_RIDS
+
+
 def sid_to_name(sid: str):
-    if sid.endswith("-500"):
-        return "Administrator"
-    if sid.endswith("-501"):
-        return "Guest"
-    if sid.endswith("-502"):
-        return "KRBTGT"
-    if sid.endswith("-512"):
-        return "Domain Admins"
-    if sid.endswith("-513"):
-        return "Domain Users"
-    if sid.endswith("-514"):
-        return "Domain Guests"
-    if sid.endswith("-515"):
-        return "Domain Computers"
-    if sid.endswith("-516"):
-        return "Domain Controllers"
-    if sid.endswith("-517"):
-        return "Cert Publishers"
-    if sid.endswith("-518"):
-        return "Schema Admins"
-    if sid.endswith("-519"):
-        return "Enterprise Admins"
-    if sid.endswith("-520"):
-        return "Group Policy Creator Owners"
-    if sid.endswith("-553"):
-        return "RAS and IAS Servers"
+    res = WELLKNOWN_SIDS.get(sid)
+    if res:
+        return res[0]
+    for rid, res in WELLKNOWN_RIDS.items():
+        if sid.endswith(f"-{rid}"):
+            return res[0]
 
     return sid
